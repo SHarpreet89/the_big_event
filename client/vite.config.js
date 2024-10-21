0b3ea340e5de7c203cee858b803011bccfda2573
Harpreet Singh <S.harpreet@me.com>
2024-10-21 20:23:57 +1100

    Render Deploy Test 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import path from "path"
 import { fileURLToPath } from 'url'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
