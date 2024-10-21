12a25e93cd73df5981781d1c4efa85fd793b6e54
Harpreet Singh <S.harpreet@me.com>
2024-10-21 20:01:31 +1100

    Render Deploy Test 1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,31 @@
 import path from "path"
 import { fileURLToPath } from 'url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      open: true,
+      proxy: {
+        '/graphql': {
+          target: env.VITE_API_URL,
+          secure: false,
+          changeOrigin: true,
+        }
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
       }
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
+  }
 })
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/thebigevent', {
+const devUri = 'mongodb://localhost:27017/thebigevent';
+const prodUri = process.env.MONGODB_URI;
+
+const mongoUri = process.env.NODE_ENV === 'production' ? prodUri : devUri;
+
+mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
